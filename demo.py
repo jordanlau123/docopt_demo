d3859363a2ac0f87e423aec0e3c4f4ca7d00b308
jordanlau123 <jordannlau@gmail.com>
2020-11-20 11:08:10 -0800

    q9 add main fcuntion

--- demo.py
+++ demo.py
@@ -5,7 +5,7 @@
 Usage: docopt.py <arg1> [<arg4>] --arg2=<arg2> [--arg3=<arg3>]
 
 Options:
-<arg>             Takes any value (this is a required positional argument)
+<arg1>             Takes any value (this is a required positional argument)
 [<arg4>]          Takes any value (this is a optional positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
@@ -13,5 +13,10 @@ Options:
 
 from docopt import docopt
 opt = docopt(__doc__)
-print(opt)
-print(type(opt))
+
+def main(arg, arg4, arg2, arg3):
+  print(opt)
+  print(type(opt))
+  
+if __name__ == "__main__":
+    main(opt["<arg1>"], opt["<arg4>"], opt["--arg2"], opt["--arg3"])
